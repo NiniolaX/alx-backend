d47f62bc9a00a92e39f502b4deeecd35e6025a4b
Love Afinni <loveafinni16@gmail.com>
2024-10-29 20:03:28 +0000

    Added method that paginates without data loss on deletion

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Deletion-resilient hypermedia pagination
 """
@@ -14,6 +13,9 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """
+        Initializes server instance.
+        """
         self.__dataset = None
         self.__indexed_dataset = None
 
@@ -40,4 +42,44 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        pass
+        """Returns a paginated set of data from the server without missing
+        items when page changes.
+
+        Args:
+            index (int): Current start index of returning page.
+            page_size (int): Size of the page returned.
+
+        Returns:
+            (Dict): Containing data page and some metadata.
+        """
+        assert isinstance(index, int) and index < len(self.__indexed_dataset)
+        assert isinstance(page_size, int) and page_size > 0
+
+        # Load file into cache
+        self.dataset()
+        self.indexed_dataset()
+
+        # Get dataset within specified range
+        data = []
+        start = index
+        end = index + page_size
+        for i in range(start, end, 1):
+            if i in self.__indexed_dataset:
+                row = self.__indexed_dataset.get(i)
+                data.append(row)
+
+        # Compute metadata
+        page_size = len(data)
+        index = 
+        next_index = end + 1
+        while not self.__indexed_dataset.get(next_index):
+            next_index += 1
+
+        result = {
+                "index": sorted(data)[0],
+                "next_index": next_index,
+                "page_size": page_size,
+                "data": data
+                }
+
+        return result
