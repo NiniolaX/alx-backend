d4a312450572f5a08e768399c1f56ab80f61f27f
NiniolaX <loveafinni16@gmail.com>
2024-11-02 22:47:47 +0100

    Added a BasicCache class that uses a simple dictionary as a cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""
+Contains a BasicCache class.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache
+    """
+    def __init__(self):
+        """ Initializes a BasicCache instance """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Adds an item to the cache """
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieves an item from the cache """
+        if not key:
+            return
+        return self.cache_data.get(key)
