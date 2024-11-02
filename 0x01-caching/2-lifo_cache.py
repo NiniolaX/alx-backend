ca6466f36a562aa2154fbeff80f0c646ceef3861
NiniolaX <loveafinni16@gmail.com>
2024-11-03 00:11:15 +0100

    Added LIFOCache class that represents a caching system using LIFO replacement policy

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Contains a a class LIFOCache that implements the LIFO Cache replacement
+policy.
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache class
+    """
+    def __init__(self):
+        """ Initializes a FIFOCache instance """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Inserts a new item into the cache """
+        if not key or not item:
+            return
+
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        # If cache is full, free cache by LIFO replacement policy
+        if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
+            last_key_inserted, last_item_inserted = self.cache_data.popitem()
+            print(f"DISCARD: {last_key_inserted}")
+
+        # Insert new item after deletion
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieves an item from cache """
+        if not key:
+            return
+        return self.cache_data.get(key)
