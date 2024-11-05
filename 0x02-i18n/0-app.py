8ead53719ba63cb5b5f1d2b77a5f61c447ffbd0f
NiniolaX <loveafinni16@gmail.com>
2024-11-05 13:09:40 +0100

    Added readme

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,17 @@
 #!usr/bin/env python3
 """ Flask app for i18n project """
 from flask import Flask, render_template
+from typing import F
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home() -> None:
+def home() -> str:
     """ Returns the home page """
     return render_template("0-index.html")
 
 
 if __name__ == "__main__":
     """ Start the Flask app """
-    app.run(debug=True)
+    app.run()
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,2 @@
+# 0x02-i18n
+Repository for ALX i18n project of the Backend Specialization Program.
