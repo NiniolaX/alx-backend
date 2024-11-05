c8099bd99dbaadeff22d9afc70139964f4812b3f
NiniolaX <loveafinni16@gmail.com>
2024-11-05 13:04:36 +0100

    Created a simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+""" Flask app for i18n project """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """ Returns the home page """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    """ Start the Flask app """
+    app.run(debug=True)
