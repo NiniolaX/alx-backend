997aea8c6cfae685ab5a131b0bd52332230156a0
NiniolaX <loveafinni16@gmail.com>
2024-12-02 16:23:52 +0100

    Improved readability

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,14 +1,15 @@
 // Tests connection to redis server
 import redis from 'redis';
 
+// Create a new client
 const client = redis.createClient();
 
+// Listen for error event
 client.on('error', (err) => {
   console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
+// Listen for connect event
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
-
-export default client;
